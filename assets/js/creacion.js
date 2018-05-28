006ec18a7f143b5781149e48eaa4a825d1d71fb0
marioaym <mario_097@outlook.com>
2018-05-28 00:46:34 -0500

    mayo28

---
+++ assets/js/creacion.js
@@ -0,0 +1,216 @@
+$(document).ready(function () {
+
+    var nivelCreacion = 1; //Niveles 0-Galaxia, 1-Nebulosa, 2-Sistema Solar y 3-Planeta
+
+    $("#btnConfig").on("click", function () {
+        $('#sideBarConfig').addClass('active');
+        $('#cerrarConfig').addClass('active');
+        $('#overlay').fadeIn();
+    });
+
+    $("#cerrarConfig").on("click", function () {
+        $('#sideBarConfig').removeClass('active');
+        $('#cerrarConfig').removeClass('active');
+        $('#overlay').fadeOut();
+    });
+
+    $("#btnCrearNebu").on("click", function () {
+        nivelCreacion = 1;
+    });
+    //PRUEBA ALERTIFY
+    $("#btnpruebaAle").on("click", function () {
+        alertify.confirm('<h3 class="alertify-titulo-info">Definir Camino</h3>',''
+        , function () {
+            $("#acptTerminos").attr('checked', true);
+        }, function () {
+            $("#acptTerminos").removeAttr('checked');
+        }).set({
+            labels: {
+                cancel: 'Cancelar',
+                ok: 'Guardar'
+            },
+            closable: true,
+            closableByDimmer: true,
+            reverseButtons: true
+        });
+    });
+
+    $("#btnCrearElemento").on("click", function () {
+        switch (nivelCreacion) {
+            case 1:
+                cargarFormularioSistemaSolar(true);
+                // $("#btnGuardar").parent().addClass("d-none");// ocultar botón guardar
+                nivelCreacion = 2;
+                break;
+            case 2:
+                cargarFormularioPlaneta();
+                nivelCreacion = 3;
+                break;
+        }
+    });
+
+    $("#btnCancelar").on("click", function () {
+        switch (nivelCreacion) {
+            case 1:
+                alert("yaaa");
+                nivelCreacion = 0;
+                break;
+            case 2:
+                cargarFormularioNebulosa();
+                nivelCreacion = 1;
+                break;
+            case 3:
+                cargarFormularioSistemaSolar(false);
+                nivelCreacion = 2;
+                break;
+        }
+    });
+    // $("#btnGuardar").parent().addClass("d-none");
+    $("#btnCrearElemento").click();
+    $("#btnCrearElemento").click();
+
+    function cargarFormularioNebulosa() {
+        $('.estiloSistemaSolar').addClass("d-none"); // ocultar estilos sistema solar
+
+        $("#crearTituloInfo").text("Creación de Nebulosa"); // cambiar titulo
+        $("#inputNombre").val("Nebulosa 1"); // cambiar nombre
+        $('#nebulosaEsPeligrosa').removeClass("d-none"); //mostrar check es peligrosa
+        $('.estiloNebulosa').removeClass("d-none"); // mostrar estilos nebulosa
+        $("#listaElementosTituloInfo").text("Lista de Sistemas Solares"); // cambiar texto listas
+        $("#btnCrearElemento").text("Crear Sistema Solar"); // cambiar texto botón crear
+    }
+
+    function cargarFormularioSistemaSolar(desdeNebulosa) {
+        if (desdeNebulosa) {
+            //De nebulosa a sistema solar
+            $('#nebulosaEsPeligrosa').addClass("d-none"); //ocultar check es peligrosa
+            $('.estiloNebulosa').addClass("d-none"); // ocultar estilos nebulosa
+        } else {
+            //De planeta a sistema Solar
+            $("#tipoPlanetaSelect").addClass("d-none"); // ocultar selección de tipo
+            $('.estiloPlaneta').addClass("d-none"); // ocultar estilos planetas 
+            $("#materialesPlaneta").addClass("d-none"); // ocultar materiales
+            $("#listaContainer").removeClass("d-none"); // mostrar lista de elementos
+            $("#btnCrearElemento").removeClass("d-none"); // mostrar boton crear elemento
+        }
+
+        $("#crearTituloInfo").text("Creación de Sistema Solar"); // cambiar titulo
+        $("#inputNombre").val("Sistema Solar 1"); // cambiar nombre
+        $('.estiloSistemaSolar').removeClass("d-none"); // mostrar estilos sistema solar
+        $("#listaElementosTituloInfo").text("Lista de Planetas"); // cambiar texto listas
+        $("#btnCrearElemento").text("Crear Planeta"); // cambiar texto botón crear
+    }
+
+    function cargarFormularioPlaneta() {
+        //De Sistema solar a Planeta
+        $('.estiloSistemaSolar').addClass("d-none"); // ocultar estilos sistema solar
+        $("#listaContainer").addClass("d-none"); // ocultar lista de elementos
+        $("#btnCrearElemento").addClass("d-none"); // ocultar boton crear elemento
+
+        $("#crearTituloInfo").text("Creación de Planeta"); // cambiar titulo
+        $("#inputNombre").val("Planeta 1"); // cambiar nombre
+        $("#tipoPlanetaSelect").removeClass("d-none"); // mostrar selección de tipo
+        $('.estiloPlaneta').removeClass("d-none"); // mostrar estilos planetas 
+        $("#materialesPlaneta").removeClass("d-none"); //mostrar materiales
+    }
+
+    //Activacion de estilo
+    $('.estiloNebulosa').on("click", function () {
+        if (!$(this).hasClass("activo")) {
+            $(".estiloNebulosa.activo").removeClass("activo");
+            $(this).addClass("activo");
+        }
+    });
+    $('.estiloSistemaSolar').on("click", function () {
+        if (!$(this).hasClass("activo")) {
+            $(".estiloSistemaSolar.activo").removeClass("activo");
+            $(this).addClass("activo");
+        }
+    });
+    $('.estiloPlaneta').on("click", function () {
+        if (!$(this).hasClass("activo")) {
+            $(".estiloPlaneta.activo").removeClass("activo");
+            $(this).addClass("activo");
+        }
+        switch ($(this).attr("data-tipoPlaneta")) {
+            case "1":
+                $("#cantIridioRango").val(1000);
+                $("#cantPlatinoRango").val(1000);
+                $("#cantPaladioRango").val(1000);
+                $("#cantEZeroRango").val(1000);
+                break;
+            case "2":
+                $("#cantIridioRango").val(5000);
+                $("#cantPlatinoRango").val(3000);
+                $("#cantPaladioRango").val(1500);
+                $("#cantEZeroRango").val(100);
+                break;
+            case "3":
+                $("#cantIridioRango").val(1000);
+                $("#cantPlatinoRango").val(3000);
+                $("#cantPaladioRango").val(5000);
+                $("#cantEZeroRango").val(7000);
+                break;
+            case "4":
+                $("#cantIridioRango").val(2000);
+                $("#cantPlatinoRango").val(5000);
+                $("#cantPaladioRango").val(3000);
+                $("#cantEZeroRango").val(3000);
+                break;
+            case "5":
+                $("#cantIridioRango").val(6000);
+                $("#cantPlatinoRango").val(7000);
+                $("#cantPaladioRango").val(5000);
+                $("#cantEZeroRango").val(1000);
+                break;
+        }
+    });
+    $('.estiloEEspacial').on("click", function () {
+        if (!$(this).hasClass("activo")) {
+            $(".estiloEEspacial.activo").removeClass("activo");
+            $(this).addClass("activo");
+        }
+    });
+    $('.estiloTTransportador').on("click", function () {
+        if (!$(this).hasClass("activo")) {
+            $(".estiloTTransportador.activo").removeClass("activo");
+            $(this).addClass("activo");
+        }
+    });
+
+    $("#selectTipoPlaneta").change(function () {
+        switch ($(this).val()) {
+            case "1":
+                $('.estiloPlaneta').removeClass("d-none"); // mostrar estilos planetas 
+                $("#materialesPlaneta").removeClass("d-none"); //mostrar materiales
+                $('.estiloEEspacial').addClass("d-none"); // ocultar estilos planetas 
+                $('.estiloTTransportador').addClass("d-none"); // ocultar estilos planetas 
+                break;
+            case "2":
+                $('.estiloEEspacial').removeClass("d-none"); // mostrar estilos planetas 
+                $('.estiloPlaneta').addClass("d-none"); // mostrar estilos planetas 
+                $('.estiloTTransportador').addClass("d-none"); // mostrar estilos teletransportador 
+                $("#materialesPlaneta").addClass("d-none"); // ocultar materiales
+                break;
+            case "3":
+                $('.estiloTTransportador').removeClass("d-none"); // mostrar estilos planetas 
+                $('.estiloPlaneta').addClass("d-none"); // mostrar estilos planetas 
+                $('.estiloEEspacial').addClass("d-none"); // mostrar estilos estacion espacial 
+                $("#materialesPlaneta").addClass("d-none"); // ocultar materiales
+                break;
+        }
+    });
+
+    $("#cantIridioRango").change(function () {
+        $("#cantIridio").text(this.value + "T");
+    });
+    $("#cantPlatinoRango").change(function () {
+        $("#cantPlatino").text(this.value + "T");
+    });
+    $("#cantPaladioRango").change(function () {
+        $("#cantPaladio").text(this.value + "T");
+    });
+    $("#cantEZeroRango").change(function () {
+        $("#cantEZero").text(this.value + "T");
+    });
+});
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,4 @@
 $(document).ready(function () {
-    $("#btnConfig").on("click", function () {
-        $('#sideBarConfig').addClass('active');
-        $('#cerrarConfig').addClass('active');
-        $('#overlay').fadeIn();
-    });
-
-    $("#cerrarConfig").on("click", function () {
-        $('#sideBarConfig').removeClass('active');
-        $('#cerrarConfig').removeClass('active');
-        $('#overlay').fadeOut();
-    });
     
     var ancho = $(window).width();
     var alto = $(window).height();
