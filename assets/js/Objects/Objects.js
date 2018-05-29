afcb078fbcab696b2dd996f4d32fa92b43c9c427
marioaym <mario_097@outlook.com>
2018-05-28 22:08:08 -0500

    Mario

--- assets/js/Objects/Objects.js
+++ assets/js/Objects/Objects.js
@@ -1,45 +1,44 @@
-
 class Galaxia {
     constructor(nombre) {
         this.nombre = nombre;
         this.Nebulosas = [];
-        this.lineas=[];
-        this.matrizAdy=[];
+        this.lineas = [];
+        this.matrizAdy = [];
     }
 }
 
 class Nebulosa {
-    constructor(id,nombre, ubicacionX, ubicacionY,teletrasportador,peligroso) {
+    constructor(id, nombre, ubicacionX, ubicacionY, teletrasportador, peligroso) {
         this.id = id;
-        this.depCombustibleGastado=false;
-        this.teletrasportador=teletrasportador;
+        this.depCombustibleGastado = false;
+        this.teletrasportador = teletrasportador;
         this.nombre = nombre;
-        this.peligroso=peligroso;
+        this.peligroso = peligroso;
         this.ubicacionX = ubicacionX;
         this.ubicacionY = ubicacionY;
         this.sistemasPlanetarios = [];
-        this.lineas=[];
-        this.matrizAdy=[];
+        this.lineas = [];
+        this.matrizAdy = [];
     }
 
 }
 
 class SistemasPlanetarios {
-    constructor(id,nombre, ubicacionX, ubicacionY) {
+    constructor(id, nombre, ubicacionX, ubicacionY) {
         this.id = id;
         this.nombre = nombre;
         this.ubicacionX = ubicacionX;
         this.ubicacionY = ubicacionY;
         this.Planetas = [];
-        this.lineas=[];
+        this.lineas = [];
     }
 }
 
 class Planetas {
-    constructor(id,nombre, iridio,platino,paladio, elementoCero,tipo,ubicacionX,ubicacionY) {
+    constructor(id, nombre, iridio, platino, paladio, elementoCero, tipo, ubicacionX, ubicacionY) {
         this.id = id;
         this.nombre = nombre;
-        this.tipo=tipo;
+        this.tipo = tipo;
         this.ubicacionX = ubicacionX;
         this.ubicacionY = ubicacionY;
         this.iridio = iridio;
--- assets/js/main.js
+++ assets/js/main.js
@@ -9,20 +9,25 @@ $(document).ready(function () {
     });
 
     $("#costo").on("click", function () {
-        !activarCosto?activarCosto=true:activarCosto=false;
+        !activarCosto ? activarCosto = true : activarCosto = false;
         console.log(activarCosto);
     });
 
-    var activarCosto=false;
-    var posicionesLineas=[];
-    var elementoSeleccionado=[];
+    var activarCosto = false;
+    var posicionesLineas = [];
+    var elementoSeleccionado = [];
 
     var ancho = $(window).width();
     var alto = $(window).height();
 
     var galaxia = new Galaxia("Galaxia");
 
-    var game = new Phaser.Game(ancho,alto,Phaser.AUTO,'gameContainer',{preload:preload,create:create,update:update,render:render});
+    var game = new Phaser.Game(ancho, alto, Phaser.AUTO, 'gameContainer', {
+        preload: preload,
+        create: create,
+        update: update,
+        render: render
+    });
 
     function preload() {
         game.load.image('image', 'assets/images/favicon.png');
@@ -35,7 +40,7 @@ $(document).ready(function () {
     }
 
     function update() {
-        updateLineGalaxia(galaxia.lineas,galaxia.Nebulosas);
+        updateLineGalaxia(galaxia.lineas, galaxia.Nebulosas);
         //line1.fromSprite(handle1, handle2, false);
     }
 
@@ -43,18 +48,20 @@ $(document).ready(function () {
         renderLines(galaxia.lineas);
     }
 
-    function startGame(){
-        game.input.mouse.capture=true;
+    function startGame() {
+        game.input.mouse.capture = true;
     }
 
     function newNebulosa() {
         var idNeb = galaxia.Nebulosas.length;
-        var nebulosa = new Nebulosa(idNeb,"NOMBRE"+idNeb,100,100,false,false);
+        var nebulosa = new Nebulosa(idNeb, "NOMBRE" + idNeb, 100, 100, false, false);
         var imagePrueba = game.add.sprite(100, 100, 'image');
-        imagePrueba.width=30;
-        imagePrueba.height=30;
-        imagePrueba.inputEnabled=true;
-        imagePrueba.events.onInputUp.add(clickSprite,{idNeb:idNeb},this);
+        imagePrueba.width = 30;
+        imagePrueba.height = 30;
+        imagePrueba.inputEnabled = true;
+        imagePrueba.events.onInputUp.add(clickSprite, {
+            idNeb: idNeb
+        }, this);
         imagePrueba.input.enableDrag();
         galaxia.Nebulosas.push(nebulosa);
         addColumnMatrizAdy(galaxia.matrizAdy);
@@ -62,38 +69,39 @@ $(document).ready(function () {
 
 
     function clickSprite(sprite, pointer) {
-        if(activarCosto){
+        if (activarCosto) {
             posicionesLineas.push(pointer.position.x);
             posicionesLineas.push(pointer.position.y);
             elementoSeleccionado.push(this.idNeb);
-            if(posicionesLineas.length===4){
+            if (posicionesLineas.length === 4) {
                 var line = new Phaser.Line(posicionesLineas[0], posicionesLineas[1], posicionesLineas[2], posicionesLineas[3]);
                 galaxia.lineas.push(line);
                 printLines(galaxia.lineas);
-                galaxia.matrizAdy[elementoSeleccionado[0]][elementoSeleccionado[1]]=1;//en vez de 1 va el costo
-                galaxia.matrizAdy[elementoSeleccionado[1]][elementoSeleccionado[0]]=1;//en vez de 1 va el costo
-                elementoSeleccionado=[];
-                posicionesLineas=[];
+                galaxia.matrizAdy[elementoSeleccionado[0]][elementoSeleccionado[1]] = 1; //en vez de 1 va el costo
+                galaxia.matrizAdy[elementoSeleccionado[1]][elementoSeleccionado[0]] = 1; //en vez de 1 va el costo
+                elementoSeleccionado = [];
+                posicionesLineas = [];
                 console.log(galaxia.matrizAdy);
             }
-        }else{
+        } else {
             console.log(galaxia.Nebulosas);
         }
 
     }
 
     function printLines(arrayLineas) {
-        arrayLineas.forEach(function(line) {
+        arrayLineas.forEach(function (line) {
             game.debug.geom(line);
         });
     }
 
-    function renderLines(arrayLineas){
-        arrayLineas.forEach(function(line) {
+    function renderLines(arrayLineas) {
+        arrayLineas.forEach(function (line) {
             game.debug.geom(line);
         });
     }
-    function updateLineGalaxia(arrayLineas,arrayNebulosas){
+
+    function updateLineGalaxia(arrayLineas, arrayNebulosas) {
 
     }
 
